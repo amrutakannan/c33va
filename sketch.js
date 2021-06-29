12af278f5bc8e3f18631e7a23d6725ac2d0dd0d1
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-06-29 23:59:55 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -56,20 +56,9 @@ function draw() {
 
   showBoats();
 
+ 
   for (var i = 0; i < balls.length; i++) {
     showCannonBalls(balls[i], i);
-    for (var j = 0; j < boats.length; j++) {
-      if (balls[i] !== undefined && boats[j] !== undefined) {
-        var collision = Matter.SAT.collides(balls[i].body, boats[j].body);
-        if (collision.collided) {
-          boats[j].remove(j);
-
-          Matter.World.remove(world, balls[i].body);
-          balls.splice(i, 1);
-          i--;
-        }
-      }
-    }
   }
 
   cannon.display();
