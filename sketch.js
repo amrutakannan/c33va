5487c26d42b62f54064aa67af5c897f0689a1019
amrutakannan <75526814+amrutakannan@users.noreply.github.com>
2023-06-21 15:14:31 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -59,6 +59,19 @@ function draw() {
  
   for (var i = 0; i < balls.length; i++) {
     showCannonBalls(balls[i], i);
+        for (var j = 0; j < boats.length; j++) {
+      if (balls[i] !== undefined && boats[j] !== undefined) {
+        var collision = Matter.SAT.collides(balls[i].body, boats[j].body);
+        if (collision.collided) {
+          boats[j].remove(j);
+
+          Matter.World.remove(world, balls[i].body);
+          balls.splice(i, 1);
+          i--;
+          
+        }
+      } 
+    }
   }
 
   cannon.display();
